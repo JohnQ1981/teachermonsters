d5768af726a592e5b96557ee2fe6a66fe79941c6
John Qosimi <john.qosimi@cat.com>
2022-09-22 16:57:26 -0500

    added webpack

--- teachermonsters/monsters/src/App.js
+++ teachermonsters/monsters/src/App.js
@@ -36,7 +36,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="app-title">Hello Monsters1</h1>
+      <h1 className="app-title">Hello Monsters</h1>
       <h2 className="app-title">Title as you type: {title}</h2>
 
       <SearchBox
