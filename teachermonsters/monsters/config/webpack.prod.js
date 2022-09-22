b6375da73f77d3006b6ce50d3c73fb26aa8b9550
John Qosimi <john.qosimi@cat.com>
2022-09-22 16:26:28 -0500

    added webpack

--- teachermonsters/monsters/config/webpack.prod.js
+++ teachermonsters/monsters/config/webpack.prod.js
@@ -1,25 +1,25 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
-  mode: 'production',
+  mode: "production",
   output: {
-    filename: '[name].[contenthash].js',
-    publicPath: '/monsters/latest/',
+    filename: "[name].[contenthash].js",
+    publicPath: "/monsters/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
-    //   remotes: {
-    //     companymanagement: `companymanagement@${domain}/companymanagement/latest/remoteEntry.js`,
-    //     testmanagement: `testmanagement@${domain}/testmanagement/latest/remoteEntry.js`,
-    //     rolemanagement: `rolemanagement@${domain}/rolemanagement/latest/remoteEntry.js`,
-    //     useraccountmanagement: `useraccountmanagement@${domain}/useraccountmanagement/latest/remoteEntry.js`,
-    //   },
+      name: "monsters",
+      //   remotes: {
+      //     companymanagement: `companymanagement@${domain}/companymanagement/latest/remoteEntry.js`,
+      //     testmanagement: `testmanagement@${domain}/testmanagement/latest/remoteEntry.js`,
+      //     rolemanagement: `rolemanagement@${domain}/rolemanagement/latest/remoteEntry.js`,
+      //     useraccountmanagement: `useraccountmanagement@${domain}/useraccountmanagement/latest/remoteEntry.js`,
+      //   },
       shared: packageJson.dependencies,
     }),
   ],
